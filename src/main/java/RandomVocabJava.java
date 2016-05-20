6213c66d23cc3a2f4bbf6d7656a88d59f0318827
Rishi <singhal.rishi@gmail.com>
2016-05-20 18:12:27 +0530

    Updated

--- src/main/java/RandomVocabJava.java
+++ src/main/java/RandomVocabJava.java
@@ -1,5 +1,6 @@
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.function.Function;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
@@ -26,9 +27,11 @@ import nodes.SymmetricRectifier;
 import nodes.Vectorizer;
 import nodes.ZCAWhitener;
 import scala.Function1;
+import scala.Option;
 import scala.Tuple2;
 import utils.Image;
 import utils.Stats;
+import utils.ZCAWhitenerExtended;
 
 /**
  * Takes the RandomVocab Scala example present 
@@ -37,69 +40,73 @@ import utils.Stats;
  */
 public class RandomVocabJava {
 
-  public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-	// Setup Spark context to run it locally  
-    SparkConf conf = new SparkConf().setAppName("LinearPixelsJava");
-    conf.setMaster("local");
-    JavaSparkContext jsc = new JavaSparkContext(conf);
-    SparkContext sc = JavaSparkContext.toSparkContext(jsc);
-    
-    // Setup the constants. Some can be overridden by arguments being passed
-    String trainDataFile = null;
-    String testDataFile = null;
-    
-    int numFilters=200;
-    int patchSize = 6;
-    double alpha = 0.25;
-    int poolSize = 14;
-    int poolStride = 13;
-    float lambda = 10; 
-    double sample = 0.2;
-    boolean isSample = false;
-    int numClasses = 10;
-    int imageSize = 32;
-    int numChannels = 3;
-    		
-    if(args.length<4)
-    {
-    	System.out.println("Using default parameter values");
-    	trainDataFile = "C:\\rishi\\Big Data\\ampcamp\\ampcamp-pipelines\\data\\cifar_train.bin";
-    	testDataFile = "C:\\rishi\\Big Data\\ampcamp\\ampcamp-pipelines\\data\\cifar_test.bin";
-    } else {
-    	trainDataFile = args[0]; 
-    	testDataFile = args[1];
-    	numFilters = Integer.parseInt(args[2]);
-    	lambda = Integer.parseInt(args[3]);
-    }
-    
-    if(args.length > 4) {
-    	sample = Double.parseDouble(args[4]);
-    	isSample = true;
-    }
-    
-    // Read the data files
-    RDD<LabeledImage> trainIimgRdd = new CifarParser().apply(new Tuple2<SparkContext, String>(sc, trainDataFile));
-    RDD<LabeledImage> trainData = null;
-    
-    if(isSample)
-    	trainData = new CachingNode<LabeledImage>("TrainImageCache")
-    				.apply(new DownSampler<LabeledImage>(sample).apply(trainIimgRdd));
-    else
-    	trainData = new CachingNode<LabeledImage>("TrainImageCache").apply(trainIimgRdd);
-    
-    RDD<LabeledImage> testIimgRdd = new CifarParser().apply(new Tuple2<SparkContext, String>(sc, testDataFile));
-    RDD<LabeledImage> testData = new CachingNode<LabeledImage>("TestImageCache").apply(testIimgRdd);
+		// Setup Spark context to run it locally  
+		SparkConf conf = new SparkConf().setAppName("RandomVocabJava");
+		conf.setMaster("local");
+		JavaSparkContext jsc = new JavaSparkContext(conf);
+		SparkContext sc = JavaSparkContext.toSparkContext(jsc);
 
-    // Check the total number of images in the dataset
-    JavaRDD<LabeledImage> trainDataRdd = trainData.toJavaRDD();
-    System.out.println("Total Images:"+trainDataRdd.count());
-    
-    // Create the train featurizer
-    FloatMatrix filters = FloatMatrix.randn(numFilters, patchSize*patchSize*numChannels);
-    float[][] filterArray = filters.toArray2();
-    
-    /*
+		// Setup the constants. Some can be overridden by arguments being passed
+		String trainDataFile = null;
+		String testDataFile = null;
+
+		int numClasses = 10;
+		int imageSize = 32;
+		int numChannels = 3;
+
+		int numFilters=200;
+		int patchSize = 6;
+
+		float alpha = 0.25f; 
+
+		int poolSize = 14;
+		int poolStride = 13;
+
+		float lambda = 10; 
+		double sample = 0.2;    
+		boolean isSample = true;
+
+		if(args.length<4)
+		{
+			System.out.println("Using default parameter values");
+			trainDataFile = "C:\\rishi\\Big Data\\ampcamp\\ampcamp-pipelines\\data\\cifar_train.bin";
+			testDataFile = "C:\\rishi\\Big Data\\ampcamp\\ampcamp-pipelines\\data\\cifar_test.bin";
+		} else {
+			trainDataFile = args[0]; 
+			testDataFile = args[1];
+			numFilters = Integer.parseInt(args[2]);
+			lambda = Integer.parseInt(args[3]);
+		}
+
+		if(args.length > 4) {
+			sample = Double.parseDouble(args[4]);
+			isSample = true;
+		}
+
+		// Read the train data files
+		RDD<LabeledImage> trainData = null;
+
+		if(isSample)
+			trainData = new CachingNode<LabeledImage>("TrainImageCache")
+			.apply(new DownSampler<LabeledImage>(sample)
+					.apply(new CifarParser()
+							.apply(new Tuple2<SparkContext, String>(sc, trainDataFile))));
+		else
+			trainData = new CachingNode<LabeledImage>("TrainImageCache")
+			.apply(new CifarParser()
+					.apply(new Tuple2<SparkContext, String>(sc, trainDataFile)));
+
+		// Check the total number of images in the dataset
+		JavaRDD<LabeledImage> trainDataRdd = trainData.toJavaRDD();
+		System.out.println("Total Images:"+trainDataRdd.count());
+
+		// Create the train featurizer
+		FloatMatrix filters = FloatMatrix.randn(numFilters, patchSize*patchSize*numChannels);
+		float[][] filterArray = filters.toArray2();
+
+		/*
     val featurizer =
     	      ImageExtractor
     	        .andThen(new Convolver(sc, filterArray, imageSize, imageSize, numChannels, None, true))
@@ -110,36 +117,47 @@ public class RandomVocabJava {
     	        .andThen(new FeatureNormalize)
     	        .andThen(new InterceptAdder)
     	        .andThen(new CachingNode)
-    */
-    
-    RDD<Image> t = new SymmetricRectifier(0, 0)
-    				.apply(new Convolver(sc, filterArray, imageSize, imageSize, numChannels, null, false, 10.0)
-    						.apply(ImageExtractor.apply(trainData)));	   
-    
-    RDD<float[]> trainFeatures = new CachingNode<float[]>("featureCache")
-    								.apply(new InterceptAdder()
-    										.apply(new FeatureNormalize((float) Math.exp(-12), -1)
-    												.apply(new ImageVectorizer()
-    														.apply(new Pooler(poolStride, poolSize, null, null)
-    																.apply(t)))));
-    
-    RDD<float[]> trainLabels = new CachingNode<float[]>("labelCache")
-								.apply(new ClassLabelIndicatorsFromIntLabels(10)
-										.apply(LabelExtractor.apply(trainData)));
-    
-    // Train the model & get the error
-    LinearMapper model = LinearMapper.trainWithL2(trainFeatures, trainLabels, lambda);    
-    float trainError = Stats.classificationError(model.apply(trainFeatures), trainLabels, 1);
-    System.out.println("Error on training data is:"+trainError); 
-    
-    // Test the model created
+		 */
+
+		RDD<Image> t = new SymmetricRectifier(0, alpha)
+				.apply(new Convolver(sc, filterArray, imageSize, imageSize, numChannels, Option.apply(null), true, 10.0)
+						.apply(ImageExtractor.apply(trainData)));
+
+		RDD<float[]> trainFea = new CachingNode<float[]>("featureCache")
+				.apply(new ImageVectorizer()
+						.apply(new MyPooler(poolStride, poolSize)
+								.apply(new SymmetricRectifier(0, alpha)
+										.apply(new Convolver(sc, filterArray, imageSize, imageSize, numChannels, Option.apply(null), true, 10.0)
+												.apply(ImageExtractor.apply(trainData))))));
+
+
+		RDD<float[]> trainFeatures = new CachingNode<float[]>("featureCache1")
+										.apply(new InterceptAdder()
+												.apply(new FeatureNormalize(1e-12f, -1)
+														.apply(trainFea)));
+
+		RDD<float[]> trainLabels = new CachingNode<float[]>("labelCache")
+				.apply(new ClassLabelIndicatorsFromIntLabels(numClasses)
+						.apply(LabelExtractor.apply(trainData)));
+
+		// Train the model & get the error
+		LinearMapper model = LinearMapper.trainWithL2(trainFeatures, trainLabels, lambda);    
+		float trainError = Stats.classificationError(model.apply(trainFeatures), trainLabels, 1);
+		System.out.println("Error on training data is:"+trainError); 
+
+		// Test the model created
+		/*
+    RDD<LabeledImage> testIimgRdd = new CifarParser().apply(new Tuple2<SparkContext, String>(sc, testDataFile));
+    RDD<LabeledImage> testData = new CachingNode<LabeledImage>("TestImageCache").apply(testIimgRdd);
+
     // Create the test featurizer & test labels
     RDD<float[]> testFeatures = Vectorizer.apply(GrayScaler.apply(ImageExtractor.apply(testData)));
-    RDD<float[]> testLabels = new ClassLabelIndicatorsFromIntLabels(10).apply(LabelExtractor.apply(testData));
+    RDD<float[]> testLabels = new ClassLabelIndicatorsFromIntLabels(numClasses).apply(LabelExtractor.apply(testData));
 
     float testError = Stats.classificationError(model.apply(testFeatures), testLabels, 1);
-    System.out.println("Error on test data is:"+testError);    
-  }
+    System.out.println("Error on test data is:"+testError);
+		 */    
+	}
 }
 
 
@@ -195,7 +213,7 @@ class pixelFunction implements Function1,Serializable{
 	@Override
 	public void apply$mcVD$sp(double d) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -231,7 +249,7 @@ class pixelFunction implements Function1,Serializable{
 	@Override
 	public void apply$mcVF$sp(float f) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -267,7 +285,7 @@ class pixelFunction implements Function1,Serializable{
 	@Override
 	public void apply$mcVI$sp(int i) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -303,9 +321,9 @@ class pixelFunction implements Function1,Serializable{
 	@Override
 	public void apply$mcVJ$sp(long l) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
 
 class poolFunction implements Function1<float[], Object>,Serializable{
@@ -319,10 +337,10 @@ class poolFunction implements Function1<float[], Object>,Serializable{
 	@Override
 	public Object apply(float[] arg0) {
 		float sum = 0;
-		
+
 		for(int i =0; i<arg0.length; i++)
 			sum=sum+arg0[i];
-		
+
 		return sum;
 	}
 
@@ -365,7 +383,7 @@ class poolFunction implements Function1<float[], Object>,Serializable{
 	@Override
 	public void apply$mcVD$sp(double d) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -401,7 +419,7 @@ class poolFunction implements Function1<float[], Object>,Serializable{
 	@Override
 	public void apply$mcVF$sp(float f) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -437,7 +455,7 @@ class poolFunction implements Function1<float[], Object>,Serializable{
 	@Override
 	public void apply$mcVI$sp(int i) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -473,7 +491,7 @@ class poolFunction implements Function1<float[], Object>,Serializable{
 	@Override
 	public void apply$mcVJ$sp(long l) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	  
+
 }
